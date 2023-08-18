f86c600f89d670c502e6bc1fe0593f1bdd3a1e43
LuQmu5 <luqmu5.1007@gmail.com>
2023-08-18 16:14:20 +0400

    1

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -22,7 +22,7 @@ public class PlayerInput : MonoBehaviour
             DashKeyPressed?.Invoke();
         }
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
             AttackKeyPressed?.Invoke();
         }
