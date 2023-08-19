a3c54456175fd69223859dd87eafe9c6819c0c23
LuQmu5 <luqmu5.1007@gmail.com>
2023-08-19 17:16:05 +0400

    1

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D _patrolAreaCollider;
+    [SerializeField] private Transform _attackPoint;
     [SerializeField] private float _speed = 2;
+    [SerializeField] private float _attackRange = 0.5f;
 
     private Animator _animator;
     private Rect _patrolArea;
@@ -26,7 +28,10 @@ public class EnemyController : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(Patroling());
+        // StartCoroutine(Patroling());
+        // StartCoroutine(RayCasting());
+        StartCoroutine(Detecting());
+        // StartCoroutine(CheckingPlayerInArea());
     }
 
     public void PlayAnimation(string name)
@@ -34,25 +39,75 @@ public class EnemyController : MonoBehaviour
         _animator.Play(name);
     }
 
+    private IEnumerator CheckingPlayerInArea()
+    {
+        var player = FindObjectOfType<PlayerController>().transform;
+
+        while (true)
+        {
+            if (player.transform.position.x > _patrolArea.xMax || 
+                player.transform.position.x < _patrolArea.xMin ||
+                player.transform.position.y > _patrolArea.yMax ||
+                player.transform.position.y < _patrolArea.yMin)
+            {
+                print("out of area");
+            }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator Detecting()
+    {
+        float boxAngle = 0;
+        Vector2 boxSize = new Vector2(_patrolArea.width, _patrolArea.height);
+
+        while (true)
+        {
+            var hits = Physics2D.OverlapBoxAll(transform.position, boxSize, boxAngle);
+
+            foreach (var hit in hits)
+            {
+                if (hit.TryGetComponent(out PlayerController player))
+                {
+                    print("Detected!");
+                    break;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator RayCasting()
+    {
+        while (true)
+        {
+            var hit = Physics2D.Raycast(_attackPoint.position, transform.right, _attackRange);
+            Debug.DrawRay(_attackPoint.position, transform.right * _attackRange, Color.red);
+
+            if (hit.collider != null && hit.collider.TryGetComponent(out PlayerController player))
+            {
+                print("attack");
+            }
+
+            yield return null;
+        }
+    }
+
     private IEnumerator Patroling()
     {
         var destinationOffset = 0.1f;
         var minWaitTime = 2f;
         var maxWaitTime = 4f;
+        Vector3 normalRotation = Vector3.zero;
+        Vector3 flippedRotation = new Vector3(0, 180, 0);
 
         while (true)
         {
             var destination = new Vector3(Random.Range(_patrolArea.xMin, _patrolArea.xMax), transform.position.y);
 
-            if (destination.x > transform.position.x)
-            {
-                transform.rotation = Quaternion.Euler(0, 0, 0);
-            }
-            else
-            {
-                transform.rotation = Quaternion.Euler(0, 180, 0);
-            }
-
+            transform.eulerAngles = destination.x > transform.position.x? normalRotation : flippedRotation;
             _animator.Play(AnimationNames.Run.ToString());
 
             while (Vector2.Distance(transform.position, destination) > destinationOffset)
--- Assets/Scripts/UI/BackgroundFollower.cs
+++ Assets/Scripts/UI/BackgroundFollower.cs
@@ -13,26 +13,32 @@ public class BackgroundFollower : MonoBehaviour
     private float _offsetX;
     private float _lastPositionX;
 
+    private void OnEnable()
+    {
+        PlayerInput.MoveKeyPressing += OnMoveKeyPressing;   
+    }
+
+    private void OnDisable()
+    {
+        PlayerInput.MoveKeyPressing -= OnMoveKeyPressing;
+    }
+
     private void Start()
     {
         _background = GetComponent<RawImage>();
         _lastPositionX = _followTarget.position.x;
     }
 
-    private void Update()
+    private void OnMoveKeyPressing(Vector2 direction)
     {
-        if (_lastPositionX == _followTarget.position.x)
+        if (_lastPositionX == _followTarget.position.x || direction == Vector2.zero)
             return;
 
-        var direction = _lastPositionX < _followTarget.position.x ? 1 : -1;
         _lastPositionX = _followTarget.position.x;
-
-        _offsetX += Time.deltaTime * _speed * direction;
+        _offsetX += Time.deltaTime * _speed * direction.x;
 
         if (_offsetX >= 1)
-        {
             _offsetX = 0;
-        }
 
         _background.uvRect = new Rect(_offsetX, _background.uvRect.y, _background.uvRect.width, _background.uvRect.height);
     }
